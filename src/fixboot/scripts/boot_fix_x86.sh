82fa5af58e5b58f6ce9ba48dd63a0a8abc5f686d
zccrs <ccrr1314@live.com>
2017-10-18 14:35:37 +0800

    Fix not execute "grub-intall"
    Change-Id: I8d4e0f806978d43ee358533de4a361b37fb9221d

--- src/fixboot/scripts/boot_fix_x86.sh
+++ src/fixboot/scripts/boot_fix_x86.sh
@@ -12,6 +12,7 @@ config_item() {
   eval echo "\$$1"
 }
 
+DI_BOOTLOADER=$1
 disk_is_efi=$2
 repo_path=$3
 
@@ -44,75 +45,92 @@ else
     fi
 fi
 
+set -e
+
+#install pageack
 if [[ $disk_is_efi != $system_is_efi ]]; then
     cp /etc/apt/sources.list /tmp/sources.list.bak
+    set +e
     echo "deb file://${repo_path} unstable main contrib non-free" > /etc/apt/sources.list
     apt-get update
+    echo "restore sources.list file"
+    mv -f /tmp/sources.list.bak /etc/apt/sources.list
+    set -e
 
     export DEBIAN_FRONTEND="noninteractive"
 
     if [[ $disk_is_efi == 0 ]]; then
-        DI_BOOTLOADER=$1
         echo "INFO: Detected legacy machine, installing grub to ${DI_BOOTLOADER}"
         apt-get -y -o Dpkg::Options::="--force-confdef" \
             -o Dpkg::Options::="--force-confold" --no-install-recommends \
             --allow-unauthenticated install grub-pc
-
-        grub-install --no-floppy ${DI_BOOTLOADER} --target=i386-pc --force|| \
-            error "grub-install failed! ${DI_BOOTLOADER}" 1
     else
-        BOOTLOADER_ID="$(config_item GRUB_DISTRIBUTOR | tr A-Z a-z | cut -d' ' -f1)"
-        BOOTLOADER_ID="${BOOTLOADER_ID:-deepin}"
-
         if [[ `uname -m` == "x86_64" ]]; then
             # Clover efi loader cannot use grub.efi correctly,
             # so we may patch grub or use grub.efi.signed.
             apt-get install --no-install-recommends -y --allow-unauthenticated \
                 shim-signed grub-efi-amd64-signed efibootmgr
-
-            # uefi-secure-boot options is enabled by default
-            grub-install --target=x86_64-efi --uefi-secure-boot \
-                --efi-directory=/boot/efi --bootloader-id="${BOOTLOADER_ID}" \
-                --recheck || error "grub-install failed with efi64 ${BOOTLOADER_ID}" 1
-
-            # Copy signed grub efi file.
-            [ -d /boot/efi/EFI/ubuntu ] || mkdir -p /boot/efi/EFI/ubuntu
-            cp /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/ubuntu/
-            [ -d /boot/efi/EFI/boot ] || mkdir -p /boot/efi/EFI/boot
-            cp -f /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/boot/
-
-            # Backup fallback efi first.
-            fallback_efi=/boot/efi/EFI/boot/bootx64.efi
-            fallback_efi_bak="${fallback_efi}-$(date +%s).bak"
-            [ -f "${fallback_efi}" ] && cp "${fallback_efi}" "${fallback_efi_bak}"
-            # Override fallback efi with shim.
-            cp /boot/efi/EFI/${BOOTLOADER_ID}/shim*.efi "${fallback_efi}"
         else
             apt-get install --no-install-recommends -y --allow-unauthenticated \
                 grub-efi-ia32
-            grub-install --target=i386-efi --efi-directory=/boot/efi \
-                --bootloader-id="${BOOTLOADER_ID}" --recheck || \
-                error "grub-install failed with efi! ${BOOTLOADER_ID}" 1
-
-            # Copy signed grub efi file.
-            [ -d /boot/efi/EFI/ubuntu ] || mkdir -p /boot/efi/EFI/ubuntu
-            cp /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/ubuntu/
-            [ -d /boot/efi/EFI/boot ] || mkdir -p /boot/efi/EFI/boot
-            cp -f /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/boot/
-            # Backup fallback efi first.
-            fallback_efi=/boot/efi/EFI/boot/bootia32.efi
-            fallback_efi_bak="${fallback_efi}-$(date +%s).bak"
-            [ -f "${fallback_efi}" ] && cp "${fallback_efi}" "${fallback_efi_bak}"
-            # Override fallback efi with shim.
-            cp /boot/efi/EFI/${BOOTLOADER_ID}/shim*.efi "${fallback_efi}"
         fi
     fi
+fi
 
-    echo "restore sources.list file"
-    mv -f /tmp/sources.list.bak /etc/apt/sources.list
+echo "install grub"
+if [[ $disk_is_efi == 0 ]]; then
+    set +e
+    grub-install --no-floppy ${DI_BOOTLOADER} --target=i386-pc --force|| \
+        error "grub-install failed! ${DI_BOOTLOADER}" 1
+    set -e
+else
+    BOOTLOADER_ID="$(config_item GRUB_DISTRIBUTOR | tr A-Z a-z | cut -d' ' -f1)"
+    BOOTLOADER_ID="${BOOTLOADER_ID:-deepin}"
+
+    if [[ `uname -m` == "x86_64" ]]; then
+        # uefi-secure-boot options is enabled by default
+        set +e
+        grub-install --target=x86_64-efi --uefi-secure-boot \
+            --efi-directory=/boot/efi --bootloader-id="${BOOTLOADER_ID}" \
+            --recheck || error "grub-install failed with efi64 ${BOOTLOADER_ID}" 1
+        set -e
+
+        # Copy signed grub efi file.
+        [ -d /boot/efi/EFI/ubuntu ] || mkdir -p /boot/efi/EFI/ubuntu
+        cp /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/ubuntu/
+        [ -d /boot/efi/EFI/boot ] || mkdir -p /boot/efi/EFI/boot
+        cp -f /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/boot/
+
+        # Backup fallback efi first.
+        fallback_efi=/boot/efi/EFI/boot/bootx64.efi
+        fallback_efi_bak="${fallback_efi}-$(date +%s).bak"
+        [ -f "${fallback_efi}" ] && cp "${fallback_efi}" "${fallback_efi_bak}"
+        # Override fallback efi with shim.
+        cp /boot/efi/EFI/${BOOTLOADER_ID}/shim*.efi "${fallback_efi}"
+    else
+        set +e
+        grub-install --target=i386-efi --efi-directory=/boot/efi \
+            --bootloader-id="${BOOTLOADER_ID}" --recheck || \
+            error "grub-install failed with efi! ${BOOTLOADER_ID}" 1
+        set -e
+
+        # Copy signed grub efi file.
+        [ -d /boot/efi/EFI/ubuntu ] || mkdir -p /boot/efi/EFI/ubuntu
+        cp /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/ubuntu/
+        [ -d /boot/efi/EFI/boot ] || mkdir -p /boot/efi/EFI/boot
+        cp -f /boot/efi/EFI/${BOOTLOADER_ID}/grub* /boot/efi/EFI/boot/
+        # Backup fallback efi first.
+        fallback_efi=/boot/efi/EFI/boot/bootia32.efi
+        fallback_efi_bak="${fallback_efi}-$(date +%s).bak"
+        [ -f "${fallback_efi}" ] && cp "${fallback_efi}" "${fallback_efi_bak}"
+        # Override fallback efi with shim.
+        cp /boot/efi/EFI/${BOOTLOADER_ID}/shim*.efi "${fallback_efi}"
+    fi
 fi
 
 echo "update grub"
+set +e
 update-grub || error "Failed update-grub" 2
+set -e
 
 exit 0
